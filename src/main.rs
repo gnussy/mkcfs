20fe6149391caab89b5a032a82efa53a498512cd
Pablo <PabloASHugen@protonmail.com>
2023-07-04 12:47:33 -0300

    feat: Add initial cli parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use cfs;
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version = "1.0", author = "CFS Team")]
+struct Cli {
+    #[arg(short, long, default_value_t = cfs::DEFAULT_BLOCK_SIZE)]
+    block_size: usize,
+
+    /// Sets the path to the linux block device
+    #[arg(short, long)]
+    blk_dev: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _opts: Cli = Cli::parse();
 }
